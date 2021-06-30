515db73c1ecd715a936f5fe0cf647dc52e779149
Franco Cerutti <fran94cc@gmail.com>
2021-06-30 12:37:32 -0300

    fix(createClient): fixed create client handler require

--- create-client/handler/create-client.js
+++ create-client/handler/create-client.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-use-before-define */
 const { commandMapper } = require('ebased/handler');
 
-const { inputMode } = require('ebased/handler/input/commandApi');
-const { outputMode } = require('ebased/handler/output/commandApi');
+const inputMode = require('ebased/handler/input/commandApi');
+const outputMode = require('ebased/handler/output/commandApi');
 const { createClientDomain } = require('../domain/create-client.domain');
 /**
  *
