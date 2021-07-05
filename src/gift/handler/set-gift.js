17e4c4973880cbe1471a8a78e73d20b5585c2cbe
Franco Cerutti <fran94cc@gmail.com>
2021-07-05 14:10:22 -0300

    fix(gift): fixed setGift flow

--- src/gift/handler/set-gift.js
+++ src/gift/handler/set-gift.js
@@ -4,7 +4,7 @@ const inputMode = require('ebased/handler/input/batchEventQueue');
 const outputMode = require('ebased/handler/output/batchEventConfirmation');
 const { setGiftDomain } = require('../domain/set-gift.domain');
 
-module.exports.handler = async (events, context) => {
+module.exports.handler = async (events, context) =>
   batchEventMapper(
     { events, context },
     inputMode,
@@ -12,4 +12,3 @@ module.exports.handler = async (events, context) => {
     setGiftDomain,
     outputMode
   );
-};
